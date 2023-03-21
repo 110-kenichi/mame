ad8d204fdd29a722e6d17cd431a088369b8cc21b
itoken <40394569+110-kenichi@users.noreply.github.com>
2023-03-21 23:48:31 +0900

    4.6.5.5 Supported OPN3L for G.I.M.I.C as proxy of OPNA.

---
+++ src/VSIF/VGMPlayer/Gimic/GimicManager.cs
@@ -0,0 +1,217 @@
+﻿// copyright-holders:K.Ito
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zanac.MAmidiMEmo.Gimic
+{
+
+    public static class GimicManager
+    {
+        private static Dictionary<int, Dictionary<uint, uint>> writtenDataCache;
+
+        private static object lockObject = new object();
+
+        private static NativeC86CtlWrapper wrapperClient;
+
+        private static bool initialized;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static bool IsScciInitialized
+        {
+            get
+            {
+                return initialized;
+            }
+        }
+
+        public static void TryInitializeGimmic()
+        {
+            lock (lockObject)
+            {
+                if (!initialized)
+                {
+                    if (wrapperClient == null)
+                        wrapperClient = new NativeC86CtlWrapper();
+
+                    initialized = wrapperClient.InitializeGimic() == 0 ? true : false;
+
+                    if (initialized && writtenDataCache == null)
+                        writtenDataCache = new Dictionary<int, Dictionary<uint, uint>>();
+                }
+            }
+        }
+
+        public static void TryReleaseGimic()
+        {
+            lock (lockObject)
+            {
+                if (initialized)
+                {
+                    if (wrapperClient.DeinitializeGimic() == 0)
+                    {
+                        writtenDataCache.Clear();
+
+                        initialized = false;
+                    }
+                }
+            }
+        }
+
+        public static int GetNumberOfChip()
+        {
+            lock (lockObject)
+            {
+                if (initialized)
+                    return wrapperClient.GetNumberOfChip();
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iSoundChipType"></param>
+        /// <param name="clock"></param>
+        /// <returns></returns>
+        public static int GetModuleIndex(ChipType chipType)
+        {
+            TryInitializeGimmic();
+
+            lock (lockObject)
+            {
+                if (initialized)
+                {
+                    for (int i = 0; i < GetNumberOfChip(); i++)
+                    {
+                        if (writtenDataCache.ContainsKey(i))
+                            continue;
+                        if (wrapperClient.TryGetModuleIndex((uint)i, (uint)chipType) == NativeC86CtlWrapper.C86CTL_ERR_NONE)
+                        {
+                            writtenDataCache.Add(i, new Dictionary<uint, uint>());
+                            return i;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iSoundChipType"></param>
+        /// <param name="clock"></param>
+        /// <returns></returns>
+        public static void ReleaseModule(int moduleIndex)
+        {
+            lock (lockObject)
+            {
+                if (writtenDataCache.ContainsKey(moduleIndex))
+                    writtenDataCache.Remove(moduleIndex);
+
+                if (writtenDataCache.Count == 0)
+                {
+                    TryReleaseGimic();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pChip"></param>
+        /// <param name="dAddr"></param>
+        /// <param name="pData"></param>
+        public static uint SetClock(int moduleIndex, uint clock)
+        {
+            lock (lockObject)
+            {
+                if (initialized)
+                    return wrapperClient.SetClock(moduleIndex, clock);
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pChip"></param>
+        /// <param name="dAddr"></param>
+        /// <param name="pData"></param>
+        public static void SetRegister(int moduleIndex, uint dAddr, uint pData)
+        {
+            SetRegister(moduleIndex, dAddr, pData, true);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pChip"></param>
+        /// <param name="dAddr"></param>
+        /// <param name="pData"></param>
+        /// <param name="useCache"></param>
+        public static void SetRegister(int moduleIndex, uint dAddr, uint pData, bool useCache)
+        {
+            lock (lockObject)
+            {
+                if (initialized)
+                {
+                    if (useCache)
+                    {
+                        var prevData = GetWrittenRegisterData(moduleIndex, dAddr);
+                        if (prevData != pData)
+                        {
+                            wrapperClient.SetRegisterDirect(moduleIndex, dAddr, pData);
+                            if (writtenDataCache.ContainsKey(moduleIndex))
+                                writtenDataCache[moduleIndex][dAddr] = pData;
+                        }
+                    }
+                    else
+                    {
+                        wrapperClient.SetRegisterDirect(moduleIndex, dAddr, pData);
+                        if (writtenDataCache.ContainsKey(moduleIndex))
+                            writtenDataCache[moduleIndex][dAddr] = pData;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iSoundChipType"></param>
+        /// <param name="clock"></param>
+        /// <returns></returns>
+        public static UInt32 GetWrittenRegisterData(int moduleIndex, uint addr)
+        {
+            lock (lockObject)
+            {
+                if (writtenDataCache.ContainsKey(moduleIndex))
+                    if (writtenDataCache[moduleIndex].ContainsKey(addr))
+                        return writtenDataCache[moduleIndex][addr];
+            }
+            return 0;
+
+            //return wrapperClient.GetWrittenRegisterData(pChip, addr);
+        }
+
+        public enum ChipType
+        {
+            CHIP_UNKNOWN = 0,
+            CHIP_OPNA,
+            CHIP_OPM,
+            CHIP_OPN3L,
+            CHIP_OPL3
+        };
+    }
+
+
+}
---
+++ src/VSIF/VGMPlayer/PortWriterGimic.cs
@@ -0,0 +1,155 @@
+﻿using FTD2XX_NET;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using zanac.MAmidiMEmo.Gimic;
+using static zanac.MAmidiMEmo.Gimic.GimicManager;
+
+namespace zanac.VGMPlayer
+{
+    public class PortWriterGimic : PortWriter
+    {
+        private bool disposedValue;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ftdiPort"></param>
+        public PortWriterGimic() : base("Gimic")
+        {
+        }
+
+        public int OpnaIndex
+        {
+            get;
+            private set;
+        } = -1;
+
+        public int OpmIndex
+        {
+            get;
+            private set;
+        } = -1;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iSoundChipType"></param>
+        /// <param name="clock"></param>
+        /// <returns></returns>
+        public int GetModuleIndex(ChipType chipType)
+        {
+            TryInitializeGimmic();
+
+            switch (chipType)
+            {
+                case ChipType.CHIP_OPNA:
+                    if (OpnaIndex < 0)
+                    {
+                        OpnaIndex = GimicManager.GetModuleIndex(GimicManager.ChipType.CHIP_OPNA);
+                        //HACK: OPN3L
+                        if (OpnaIndex < 0)
+                            OpnaIndex = GimicManager.GetModuleIndex(GimicManager.ChipType.CHIP_OPN3L);
+                    }
+                    return OpnaIndex;
+                case ChipType.CHIP_OPM:
+                    if (OpmIndex < 0)
+                        OpmIndex = GimicManager.GetModuleIndex(GimicManager.ChipType.CHIP_OPM);
+                    return OpmIndex;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="address"></param>
+        /// <param name="data"></param>
+        /// <param name="wait"></param>
+        public override void Write(PortWriteData[] data)
+        {
+            try
+            {
+                foreach (var dt in data)
+                {
+                    int id = 0;
+                    switch (dt.Type)
+                    {
+                        case 0: //OPNA
+                            if (OpnaIndex < 0)
+                            {
+                                OpnaIndex = GimicManager.GetModuleIndex(GimicManager.ChipType.CHIP_OPNA);
+                                //HACK: OPN3L
+                                if (OpnaIndex < 0)
+                                    OpnaIndex = GimicManager.GetModuleIndex(GimicManager.ChipType.CHIP_OPN3L);
+                            }
+                            if (OpnaIndex >= 0)
+                                GimicManager.SetRegister(id, dt.Address, dt.Data, false);
+                            break;
+                        case 1: //OPNA
+                            if (OpnaIndex < 0)
+                            {
+                                OpnaIndex = GimicManager.GetModuleIndex(GimicManager.ChipType.CHIP_OPNA);
+                                //HACK: OPN3L
+                                if (OpnaIndex < 0)
+                                    OpnaIndex = GimicManager.GetModuleIndex(GimicManager.ChipType.CHIP_OPN3L);
+                            }
+                            if (OpnaIndex >= 0)
+                                GimicManager.SetRegister(id, (uint)(dt.Address + 0x100), dt.Data, false);
+                            break;
+                        case 2: //OPM
+                            if (OpmIndex < 0)
+                                OpmIndex = GimicManager.GetModuleIndex(GimicManager.ChipType.CHIP_OPM);
+                            if (OpmIndex >= 0)
+                                GimicManager.SetRegister(id, dt.Address, dt.Data, false);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == typeof(Exception))
+                    throw;
+                else if (ex.GetType() == typeof(SystemException))
+                    throw;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                }
+                lock (LockObject)
+                {
+                    GimicManager.TryReleaseGimic();
+                }
+                disposedValue = true;
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="wait"></param>
+        public override void RawWrite(byte[] data, int wait)
+        {
+
+        }
+
+    }
+
+}
