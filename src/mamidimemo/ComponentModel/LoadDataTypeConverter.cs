1866e7a264f813946d2dfd4b893e7b51bd7f246e
itoken <40394569+110-kenichi@users.noreply.github.com>
2023-04-13 22:09:06 +0900

    Improved property display

---
+++ src/mamidimemo/ComponentModel/LoadDataTypeConverter.cs
@@ -0,0 +1,31 @@
+﻿// copyright-holders:K.Ito
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Collections;
+using System.Globalization;
+
+namespace zanac.MAmidiMEmo.ComponentModel
+{
+    /// <summary>
+    /// </summary>
+    public class LoadDataTypeConverter : TypeConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public LoadDataTypeConverter()
+        {
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                return "(Click […] to load data)";
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
---
+++ src/mamidimemo/ComponentModel/OpenFileBrowserTypeConverter.cs
@@ -0,0 +1,31 @@
+﻿// copyright-holders:K.Ito
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Collections;
+using System.Globalization;
+
+namespace zanac.MAmidiMEmo.ComponentModel
+{
+    /// <summary>
+    /// </summary>
+    public class OpenFileBrowserTypeConverter : TypeConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public OpenFileBrowserTypeConverter()
+        {
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                return "(Click […] to open the file browser)";
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
